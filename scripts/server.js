c8ba47c595ff1c150aeb5746f3210c59a2c54cb6
alexcom34 <alexandregaspari2004@gmail.com>
2026-05-21 14:45:22 -0300

    Suas alterações aqui

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 // 1. Configure aqui os dados do seu banco MySQL
 const db = mysql.createPool({
